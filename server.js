9d08705b1e732164508eb9f31bd587f750e504ac
Adrian <70490120+adrianpinon@users.noreply.github.com>
2021-02-04 14:05:22 -0600

    added server.js and routes folder for deployment

--- server.js
+++ server.js
@@ -5,3 +5,18 @@ const PORT = process.env.PORT || 8080
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology:true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
